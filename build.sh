a87d11f4d7dd038d20750a3d3ade60cd4d3ffe11
Alhammanazil <alhammanazil18@gmail.com>
2026-01-13 18:05:06 +0700

    chore: remove obsolete Vercel configuration files and build script

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install composer dependencies
-composer install --no-interaction --prefer-dist --optimize-autoloader
-
-# Install npm dependencies and build assets
-npm install
-npm run build
